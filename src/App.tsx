c9b0d493cade50f982b0957c215300e9a171563d
Septian A Tama <sfirst.tama@gmail.com>
2024-07-09 14:50:09 +0700

    implement content fetching and sentence parsing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import { Controls } from './components/Controls';
 import { CurrentlyReading } from './components/CurrentlyReading';
+import { fetchContent, parseContentIntoSentences } from './lib/content';
 
 function App() {
-  // const [sentences, setSentences] = useState<Array<string>>([]);
+  const [sentences, setSentences] = useState<Array<string>>([]);
   // const { currentWord, currentSentence, controls } = useSpeech(sentences);
+
+  const fetchData = async () => {
+    const content = await fetchContent();
+    setSentences(parseContentIntoSentences(content));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
       <h1>Text to speech</h1>
       <div>
-        <CurrentlyReading/>
+        <CurrentlyReading />
       </div>
       <div>
-        <Controls/>
+        <Controls />
       </div>
     </div>
   );
--- src/lib/content.ts
+++ src/lib/content.ts
@@ -1,15 +1,44 @@
-const API_URL = "http://localhost:5174/content";
+const API_URL = 'http://localhost:5174/content';
 
 /**
  * Fetch the content from the api
  * In case of an error, return content as "<speak><s>There was an error</s></speak>"
  */
-const fetchContent = async (url = API_URL): Promise<string> => {};
+const fetchContent = async (url = API_URL): Promise<string> => {
+  try {
+    const contentResponse = await fetch(`${API_URL}`);
+    const contentJson = (await contentResponse.json()) as { content: string };
+    return contentJson.content;
+  } catch (err) {
+    return '<speak><s>There was an error</s></speak>';
+  }
+};
 
 /**
  * Parse the content into sentences, and return an array of sentences. Look at the Readme for sample input and expected output.
  * Avoid using DOMParser for implementing this function.
  */
-const parseContentIntoSentences = (content: string) => {};
+
+const isValidContent = (content: string) => {
+  return content.startsWith('<speak>') && content.endsWith('</speak>');
+};
+
+const trimSpeakTag = (content: string) => {
+  return content.replace('<speak>', '').replace('</speak>', '');
+};
+
+const splitSentences = (content: string) => {
+  return content
+    .replaceAll('<s>', '')
+    .split('</s>')
+    .filter((sentence) => !!sentence);
+};
+
+const parseContentIntoSentences = (content: string): Array<string> => {
+  if (isValidContent(content)) {
+    return splitSentences(trimSpeakTag(content));
+  }
+  return [];
+};
 
 export { fetchContent, parseContentIntoSentences };
